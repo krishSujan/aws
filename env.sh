f39e89d7ef3c10aca9b9c999d3c78db29a20c3f9
peerclasses <peerclasses@gmail.com>
2020-12-01 16:46:50 +0100

    Test jenkins trigger

---
+++ env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cidr=10.0.0.0/22
+region=ap-south-1
---
+++ vpc-cli.sh
@@ -0,0 +1,8 @@
+#!/bin/bash/
+
+pwd=`pwd`
+
+source $pwd/env.sh
+
+
+/usr/local/bin/aws ec2 create-vpc --cidr-block $cidr --region $region
